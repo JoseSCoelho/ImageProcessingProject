3be467545a3d3687aa14659e51ca8cef5a38ead2
Miguel Fazenda <miguel@pop-os.localdomain>
2020-12-11 11:01:47 +0000

    Ransac e DLT

---
+++ ransac_dlt.m
@@ -0,0 +1,58 @@
+%generate points p1, and p2 from a random transformation
+p1 = rand(3, 10);
+
+T = rand(3, 1)
+A = rand(3);
+[u, s, v] = svd(A);
+R = u*v';
+
+p2 = R*p1 + T;
+
+
+%%add outliers
+p1 = [p1, rand(3,1), rand(3,1), rand(3,1)];
+p2 = [p2, rand(3,1), rand(3,1), rand(3,1)];
+p2 = [p2; ones(1, size(p2, 2))];
+
+%tests DLT (working fine)
+rt = dlt(p1(:, 1:4), p2(:, 1:4))
+
+% ransac to find the trasnformation
+m = myRansac(p1, p2, 1000, 0.2)
+
+function rt = dlt(p1, p2)
+    % Returns the matrix RT that contains the transformation from p2 to p1
+    rt = p1 / p2;
+end
+
+function bestModel = myRansac(p1, p2, maxIterations, threshold)
+    nPts = size(p1, 2);
+
+    bestModel = [];
+    bestScore = 0;
+    
+    for i = 1:maxIterations
+        rnd = randperm(nPts);         % Cria um vetor de numeros aleatórios nao repetidos
+        inliersHypothesis = rnd(1:4); % escolhe 4 pontos (guarda os indices) (escolhe os primeiros 4 numeros do vetor rnd)
+        ptsToTest = rnd(5:nPts);      % o resto dos pontos sao para serem testados
+     
+        model = dlt(p1(:, inliersHypothesis), p2(:, inliersHypothesis)); % Homography matrix
+        if size(model, 1) < 3
+            continue;
+        end
+        
+        %Testa cada ponto que não os inliersHypothesis para ver se são inliers deste
+        %modelo
+        p2Transformed = model*p2(:, ptsToTest);
+        norms = sqrt(sum((p1(:, ptsToTest)-p2Transformed).^2));
+        
+        %number of nodes with norm smalled than the treshold
+        score = sum(norms < threshold);
+        
+        if score > bestScore
+            bestModel = model;
+            bestScore = score;
+        end
+    end
+end
+
