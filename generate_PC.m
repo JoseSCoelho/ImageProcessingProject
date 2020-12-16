dede82884cbf6c864879475300b16b4084514df8
José Coelho <josescoelho@tecnico.ulisboa.pt>
2020-12-16 16:59:10 +0000

    J+a geramos a point cloud e altermos o match para ser nas rgb

--- generate_PC.m
+++ generate_PC.m
@@ -1,70 +1,30 @@
-function new_pc = generate_PC (depth_array, im, camera_params)
-    
-    [rows,cols]=size(depth_array);
-
-    [vect_coluna,vect_linha]= meshgrid(1:cols,1:rows);
-
-    depth_pixel_frame = [vect_coluna(:),vect_linha(:), ones(cols*rows,1)]'; 
-                            %Linhas 1 e 2 -> Pixeis; Linha 3 -> Para coordenadas homogeneas
-
-    Z = double(depth_array(:)); 
-
-    depth_pixel_frame = transpose(Z.*transpose(depth_pixel_frame));
-
-    depth_frame = camera_params.Kdepth\depth_pixel_frame; %Trás as coordenadas homogeneas para 
-                                            %o referencial da camera
-
-    depth_world = [depth_frame; ones([length(depth_frame), 1])']; %
-
-    %%%
-    % As cameras não estão alinhadas, por isso é necessário fazer a
-    % correspondencia entre cada ponto de XYZ e a cor desse ponto, guardada em
-    % 'im'. 'im' foi criado com o ponto de vista da camera RGB. Por isso os
-    % pontos XYZ não tem correspondencia direta aos pontos da camera RGB
-    % (existem pontos de XYZ sem correspondencia!)
-
-    rot_trans_matrix = [camera_params.R camera_params.T ; zeros(3,1)' 1];
-
-    rgb_world = rot_trans_matrix\depth_world; %Trás o referencial de depth para 
-                                            %o referencial de RGB
-
-    rgb_frame = rgb_world(1:3, :);
-
-    rgb_image = camera_params.Krgb*rgb_frame;
-
-    rgb_image(1,:)=rgb_image(1,:)./rgb_image(3,:); %Divide coordenada x por lamda
-    rgb_image(2,:)=rgb_image(2,:)./rgb_image(3,:); %Divide coordenada y por lamda
-
-    rgb_image(1:2,:)=round(rgb_image(1:2,:)); %Arredonda px e py, para serem pixeis inteiros
-
-    %Remove os pontos que foram captados pela camera depth, mas estao fora do
-    %alcance da RGB, e arredonda para o valor valido mais proximo
-    rgb_image(find(rgb_image<1))=1;
-    find_=find(rgb_image(1,:)>cols);
-    rgb_image(1, find_)=cols;
-    find_= find(rgb_image(2,:)>rows);
-    rgb_image(2, find_)=rows;
-
-    %Cria e preenche uma imagem RGB com os pontos fornecidos pela camera depth
-    im_virtual = zeros(rows, cols, 3);
-    for i=1:length(rgb_image)
-        im_virtual(rgb_image(2,i),rgb_image(1,i),:)=im(rgb_image(2,i), rgb_image(1,i),:);
-    end
-
-    %Partindo dos pontos alinhados entre as duas cameras, obtem-se uma
-    %PointCloud
+function new_pc = generate_PC (depth_array, match_coord, im, camera_params, var)
 
     %Lista de das cores para cada ponto
-    pc_color=ones(length(rgb_image),3);
-    for i=1:length(rgb_image)
-        pc_color(i,:)=im_virtual(rgb_image(2,i),rgb_image(1,i),:);
+    pc_color=ones(length(match_coord),3);
+    for i=1:length(match_coord)
+        if(var == 1)
+            pc_color(i,:)=im(match_coord(2,i),match_coord(1,i),:);
+        else
+            pc_color(i,:)=[255, 0, 0];
+        end
     end
-
-    %Reconverte (x,y) para (Zx, Zy, Z). Os pontos Z sempre estiveram guardados
-    %na linha 3
-    rgb_image(1,:)=rgb_image(1,:).*rgb_image(3,:); %Multiplica coordenada x por lamda
-    rgb_image(2,:)=rgb_image(2,:).*rgb_image(3,:); %Multiplica coordenada y por lamda
-
-    rgb_frame2 = (camera_params.Krgb\rgb_image)';
-    new_pc = pointCloud(rgb_frame2,'color', uint8(pc_color));
+    
+    %Passa a os pontos rgb_match paras as suas coordenadas espaciais
+    depth_matches = zeros(length(match_coord), 1);
+    for i=1:length(match_coord)
+        depth_matches(i)=depth_array(match_coord(2,i),match_coord(1,i),:);
+    end
+    world = (camera_params.Krgb\[match_coord.*depth_matches' ; depth_matches'])';
+    
+    %Elimina os pontos sem correspondencia na imagem depth
+    idx = find(world(:, 3) == 0);
+    world(idx, :) = [];
+    pc_color(idx, :) = [];
+    
+    new_pc = pointCloud(world,'color', uint8(pc_color));
+   
+    figure();
+    pcshow(new_pc);
+    hold on;
 end 
--- myRansac.m
+++ myRansac.m
@@ -9,7 +9,7 @@ function bestInliers = myRansac(p1, p2, maxIterations, threshold)
         inliersHypothesis = rnd(1:4); % escolhe 4 pontos (guarda os indices) (escolhe os primeiros 4 numeros do vetor rnd)
         ptsToTest = rnd(5:nPts);      % o resto dos pontos sao para serem testados
      
-        model = dlt(p1(:, inliersHypothesis), p2(:, inliersHypothesis)); % Homography matrix
+        model = dlt(p1(:, inliersHypothesis), p2(:, inliersHypothesis)); 
         
         %Testa cada ponto que não os inliersHypothesis para ver se são inliers deste
         %modelo
